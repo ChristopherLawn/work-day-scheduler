0ba66f981b3d6bf5fc5c300ab46b140945fe0ede
CLAW <christopher.lawn@yahoo.com>
2021-12-07 02:07:02 -0800

    added additional CSS styles, added JS functions to automatically check each hour, updated README file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# work-day-scheduler
+# JavaScript Code Quiz
+
+## Purpose
+A simple, easy-to-use daily task scheduler that color-codes each hour to let the user know which tasks are overdue and which tasks are coming up!
+
+## Built With
+* HTML
+* CSS
+* JS
+
+## Website
+https://christopherlawn.github.io/code-quiz-web-api/
+
+## Screenshot of JavaScript Code Quiz homepage
+
+## Contribution
+Made with ❤️ by Chris Lawn
+
+### ©️2021 JS Code Quiz
--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,92 @@
-$(document).ready(function() {
-    // Style site description
-    $(".lead").css('color','wheat');
+// Style site description
+$(".lead").css('color','sandybrown');
+$(".time-block").css('padding-bottom','2px');
 
-    // Display & style current day & local time
-    $("#today").text(moment().format('LLL'));
-    $("#today").css({'color': 'greenyellow', 'font-size': '25px'});
+// Use moment to display + style current day & local time in jumbotron
+$("#today").text(moment().format('LLL'));
+$("#today").css({'color': 'greenyellow', 'font-size': '25px'});
 
-    var now = moment();
-    console.log(now);
-
-    // function timeCheck() {
-    //     var currentTime = moment().hour();
-    // };
-
-    // Functions to save hourly task descriptions to localStorage
-    $("#8save").click(function() {
-        var eightInput = $("#8text").val();
-        localStorage.setItem("eightInput",eightInput);
-    });
-    $("#8am #8text").val(localStorage.getItem("eightInput"));
-
-    $("#9save").click(function() {
-        var nineInput = $("#9text").val();
-        localStorage.setItem("nineInput",nineInput);
-    });
-    $("#9am #9text").val(localStorage.getItem("nineInput"));
-
-    $("#10save").click(function() {
-        var tenInput = $("#10text").val();
-        localStorage.setItem("tenInput",tenInput);
-    });
-    $("#10am #10text").val(localStorage.getItem("tenInput"));
-
-    $("#11save").click(function() {
-        var elevenInput = $("#11text").val();
-        localStorage.setItem("elevenInput",elevenInput);
-    });
-    $("#11am #11text").val(localStorage.getItem("elevenInput"));
-
-    $("#12save").click(function() {
-        var twelveInput = $("#12text").val();
-        localStorage.setItem("twelveInput",twelveInput);
-    });
-    $("#12pm #12text").val(localStorage.getItem("twelveInput"));
-
-    $("#1save").click(function() {
-        var oneInput = $("#1text").val();
-        localStorage.setItem("oneInput",oneInput);
-    });
-    $("#1pm #1text").val(localStorage.getItem("oneInput"));
-
-    $("#2save").click(function() {
-        var twoInput = $("#2text").val();
-        localStorage.setItem("twoInput",twoInput);
-    });
-    $("#2pm #2text").val(localStorage.getItem("twoInput"));
-
-    $("#3save").click(function() {
-        var threeInput = $("#3text").val();
-        localStorage.setItem("threeInput",threeInput);
-    });
-    $("#3pm #3text").val(localStorage.getItem("threeInput"));
-
-    $("#4save").click(function() {
-        var fourInput = $("#4text").val();
-        localStorage.setItem("fourInput",fourInput);
-    });
-    $("#4pm #4text").val(localStorage.getItem("fourInput"));
-
-
-
+// Save hourly task descriptions to localStorage when "Save" button is clicked
+$("#8save").click(function() {
+    var eightInput = $("#8").val();
+    localStorage.setItem("eightInput",eightInput);
 });
+$("#8am #8").val(localStorage.getItem("eightInput"));
+
+$("#9save").click(function() {
+    var nineInput = $("#9").val();
+    localStorage.setItem("nineInput",nineInput);
+});
+$("#9am #9").val(localStorage.getItem("nineInput"));
+
+$("#10save").click(function() {
+    var tenInput = $("#10").val();
+    localStorage.setItem("tenInput",tenInput);
+});
+$("#10am #10").val(localStorage.getItem("tenInput"));
+
+$("#11save").click(function() {
+    var elevenInput = $("#11").val();
+    localStorage.setItem("elevenInput",elevenInput);
+});
+$("#11am #11").val(localStorage.getItem("elevenInput"));
+
+$("#12save").click(function() {
+    var twelveInput = $("#12").val();
+    localStorage.setItem("twelveInput",twelveInput);
+});
+$("#12pm #12").val(localStorage.getItem("twelveInput"));
+
+$("#1save").click(function() {
+    var oneInput = $("#13").val();
+    localStorage.setItem("oneInput",oneInput);
+});
+$("#1pm #13").val(localStorage.getItem("oneInput"));
+
+$("#2save").click(function() {
+    var twoInput = $("#14").val();
+    localStorage.setItem("twoInput",twoInput);
+});
+$("#2pm #14").val(localStorage.getItem("twoInput"));
+
+$("#3save").click(function() {
+    var threeInput = $("#15").val();
+    localStorage.setItem("threeInput",threeInput);
+});
+$("#3pm #15").val(localStorage.getItem("threeInput"));
+
+$("#4save").click(function() {
+    var fourInput = $("#16").val();
+    localStorage.setItem("fourInput",fourInput);
+});
+$("#4pm #16").val(localStorage.getItem("fourInput"));
+
+
+// Function to check work day scheduler hours against current time
+function checkHours() {
+        
+    // Use Moment JS to set current time
+    var currentHour = moment().hour();
+
+    // Use Moment JS to check if the current schedule hour of the day is past, present, or future
+    var timeBlocks = $(".description");
+    for (var i = 0; i < timeBlocks.length; i++) {
+        var hourID = timeBlocks[i].id;
+
+        var hourNumber = document.getElementById(timeBlocks[i].id);
+
+        if (hourID < currentHour) {
+            $(hourNumber).addClass("past");
+        };
+
+        if (hourID === currentHour) {
+            $(hourNumber).addClass("present")
+        };
+
+        if (hourID > currentHour) 
+            $(hourNumber).addClass("future")
+        };
+};
+
+// Run the function constantly to check the time
+checkHours();
